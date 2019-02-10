f9d376804bfa3973dfb0d3cba35a530e4eed7b1e
George Wu <georgewujia@gmail.com>
2019-02-09 17:25:52 -0800

    Updated function docs

--- R/contrast.R
+++ R/contrast.R
@@ -3,14 +3,15 @@ library(testthat)
 
 #' Contrast
 #'
-#' @param input_img
-#' @param intensity
-#' @param output_img
+#' @param input_img string, path of the input .png image
+#' @param intensity int, intensity of contrast enhancement, between 0 and 10, defaults to 5
+#' @param output_img string, path of the output .png image
 #'
-#' @return
+#' @return a png image at the specificed out path
 #' @export
 #'
 #' @examples
+#' contrast("./input.png", 5, "./output.png")
 
 contrast <- function(input_img, intensity=5, output_img){
 
--- R/vibrance.R
+++ R/vibrance.R
@@ -3,15 +3,15 @@ library(testthat)
 
 #' Vibrance
 #'
-#' @param input_img
-#' @param intensity
-#' @param output_img
+#' @param input_img string, path of the input .png image
+#' @param intensity int, intensity of vibrance enhancement, between 0 and 10, defaults to 5
+#' @param output_img string, path of the output .png image
 #'
-#' @return
+#' @return a png image at the specificed out path
 #' @export
 #'
 #' @examples
-
+#' vibrance("./input.png", 5, "./output.png")
 vibrance <- function(input_img, intensity=5, output_img){
 
 }
