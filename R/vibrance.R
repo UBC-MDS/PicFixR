8df68ea37e21cc551133887b503b4f776271aede
mkeyim94 <milibankeyim@gmail.com>
2019-03-01 23:12:01 -0800

    starter vibrance

--- R/vibrance.R
+++ R/vibrance.R
@@ -1,5 +1,6 @@
 library(png)
 
+
 #' Vibrance
 #'
 #' @param input_img string, path of the input .png image
@@ -11,6 +12,17 @@ library(png)
 #'
 #' @examples
 #' vibrance("./input.png", 5, "./output.png")
-vibrance <- function(input_img, intensity=5, output_img){
+vibrance <- function(input_img, intensity=5, display=F, output_img=""){
 
+  # exception handling
+  # note that other eceptions are already handled by the PNG library functions readPNG and writePNG
+  testit::assert("Please provide a valid string for the input image path.", is.character(input_img))
+  testit::assert("Please provide a valid string for the output image path.", is.character(output_img))
+  testit::assert("Please provide an intensity value between 0 and 10.", intensity <= 10 & intensity >= -10)
+
+  # load input image
+  img <- png::readPNG(input_img)
+
+  # scale pixel values from [0, 1] back to [0, 255]
+  img <- img * 255
 }
